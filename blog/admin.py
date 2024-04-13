97b6ccb49286c11254b1b7518c016d95339490c8
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2024-04-14 00:45:05 +0330

    Update admin.py
    these changes include various improvements in the admin panel, enhancing perfomance and accessibility to information

--- blog/admin.py
+++ blog/admin.py
@@ -2,16 +2,25 @@ from django.contrib import admin
 from .models import Blog , Category , Tag , Comments
 # Register your models here.
 
-# admin.site.register(Blog)
-admin.site.register(Category)
-admin.site.register(Tag)
-admin.site.register(Comments)
+class CommentInline(admin.TabularInline):
+    model = Comments
 
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title','published_at')
+    list_filter = ['published_at']
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['title','published_at']
+    list_filter = ['published_at']
+
+@admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ('title','created_at',"author")
+    list_display = ('title','created_at',"author","category")
     list_filter = ("author",)
     search_fields = ("title",)
     ordering = ('title',"author")
     date_hierarchy = "author__date_joined"
-
-admin.site.register(Blog,BlogAdmin)
+    list_select_related = ('author',)
+    inlines = [CommentInline]
